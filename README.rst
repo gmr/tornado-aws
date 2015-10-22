c0bca3e2158cbb60ccd5a792f085bbf5950cf232
Gavin M. Roy <gavinr@aweber.com>
2015-10-21 21:16:49 -0400

    Additional structural stuffs

---
+++ README.rst
@@ -0,0 +1,48 @@
+tornado-aws
+===========
+
+|Version| |Downloads| |Status| |Coverage| |License|
+
+Installation
+------------
+``tornado-aws`` may be installed via the Python package index with the tool of
+your choice. I prefer pip:
+
+.. code:: bash
+
+    pip install tornado-aws
+
+Example
+-------
+
+.. code:: python
+
+    import tornado
+    import tornado_aws
+
+Documentation
+-------------
+Documentation is available on `ReadTheDocs <https://tornado-aws.readthedocs.org>`_.
+
+Requirements
+------------
+-  `Tornado <https://tornadoweb.org>`_
+
+Version History
+---------------
+Available at https://tornado-aws.readthedocs.org/en/latest/history.html
+
+.. |Version| image:: https://img.shields.io/pypi/v/tornado-aws.svg?
+   :target: http://badge.fury.io/py/tornado-aws
+
+.. |Status| image:: https://img.shields.io/travis/gmr/tornado-aws.svg?
+   :target: https://travis-ci.org/gmr/tornado-aws
+
+.. |Coverage| image:: https://img.shields.io/codecov/c/github/gmr/tornado-aws.svg?
+   :target: https://codecov.io/github/gmr/tornado-aws?branch=master
+
+.. |Downloads| image:: https://img.shields.io/pypi/dm/tornado-aws.svg?
+   :target: https://pypi.python.org/pypi/tornado-aws
+
+.. |License| image:: https://img.shields.io/pypi/l/tornado-aws.svg?
+   :target: https://tornado-aws.readthedocs.org
---
+++ setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+CLASSIFIERS = ['Development Status :: 3 - Alpha',
+               'Intended Audience :: Developers',
+               'License :: OSI Approved :: BSD License',
+               'Operating System :: OS Independent',
+               'Programming Language :: Python :: 2',
+               'Programming Language :: Python :: 2.7',
+               'Programming Language :: Python :: 3',
+               'Programming Language :: Python :: 3.3',
+               'Programming Language :: Python :: 3.4',
+               'Programming Language :: Python :: 3.5',
+               'Programming Language :: Python :: Implementation :: CPython',
+               'Programming Language :: Python :: Implementation :: PyPy',
+               'Topic :: Communications',
+               'Topic :: Internet',
+               'Topic :: Software Development :: Libraries']
+
+DESC = 'A low-level Amazon Web Services API client for Tornado'
+
+TESTS_REQUIRE = ['nose', 'mock', 'coverage']
+
+setuptools.setup(name='tornado-aws',
+                 version='0.1.0',
+                 description=DESC,
+                 long_description=open('README.rst').read(),
+                 author='Gavin M. Roy',
+                 author_email='gavinmroy@gmail.com',
+                 url='http://tornado-aws.readthedocs.org',
+                 packages=['tornado-aws'],
+                 package_data={'': ['LICENSE', 'README.rst']},
+                 include_package_data=True,
+                 install_requires=['tornado'],
+                 tests_require=TESTS_REQUIRE,
+                 license='BSD',
+                 classifiers=CLASSIFIERS,
+                 zip_safe=True)
