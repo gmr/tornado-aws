f43722266d8eb211e5552c23e2ff45a3c6287686
Gavin M. Roy <gavinr@aweber.com>
2017-04-19 16:49:07 -0400

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+from os import path
 import setuptools
 
 CLASSIFIERS = ['Development Status :: 4 - Beta',
@@ -18,20 +19,38 @@ CLASSIFIERS = ['Development Status :: 4 - Beta',
 
 DESC = 'A low-level Amazon Web Services API client for Tornado'
 
-setuptools.setup(name='tornado-aws',
-                 version='0.6.0',
-                 description=DESC,
-                 long_description=open('README.rst').read(),
-                 author='Gavin M. Roy',
-                 author_email='gavinmroy@gmail.com',
-                 url='http://tornado-aws.readthedocs.org',
-                 packages=['tornado_aws'],
-                 package_data={'': ['LICENSE', 'README.rst',
-                                    'requires/installation.txt']},
-                 include_package_data=True,
-                 install_requires=open('requires/installation.txt').read(),
-                 extras_require={'curl': ['pycurl']},
-                 tests_require=open('requires/testing.txt').read(),
-                 license='BSD',
-                 classifiers=CLASSIFIERS,
-                 zip_safe=True)
+
+def read_requirements(name):
+    requirements = []
+    try:
+        with open(path.join('requires', name)) as req_file:
+            for line in req_file:
+                if '#' in line:
+                    line = line[:line.index('#')]
+                line = line.strip()
+                if line.startswith('-r'):
+                    requirements.extend(read_requirements(line[2:].strip()))
+                elif line and not line.startswith('-'):
+                    requirements.append(line)
+    except IOError:
+        pass
+    return requirements
+
+
+setuptools.setup(
+    name='tornado-aws',
+    version='0.6.0',
+    description=DESC,
+    long_description=open('README.rst').read(),
+    author='Gavin M. Roy',
+    author_email='gavinmroy@gmail.com',
+    url='http://tornado-aws.readthedocs.org',
+    packages=['tornado_aws'],
+    package_data={'': ['LICENSE', 'README.rst', 'requires/installation.txt']},
+    include_package_data=True,
+    install_requires=read_requirements('requires/installation.txt'),
+    extras_require={'curl': ['pycurl']},
+    tests_require=read_requirements('requires/testing.txt'),
+    license='BSD',
+    classifiers=CLASSIFIERS,
+    zip_safe=True)
