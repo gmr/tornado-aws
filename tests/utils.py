a7894a9a473f4156bd1b2be32bb870517245177b
Gavin M. Roy <gavinr@aweber.com>
2017-06-06 20:07:19 -0400

    Clear additional environment variables

--- tests/utils.py
+++ tests/utils.py
@@ -21,3 +21,6 @@ def clear_environment():
     os.environ.pop('AWS_CONFIG_FILE', None)
     os.environ.pop('AWS_SHARED_CREDENTIALS_FILE', None)
     os.environ.pop('AWS_DEFAULT_PROFILE', None)
+    os.environ.pop('AWS_ACCESS_KEY_ID', None)
+    os.environ.pop('AWS_SECRET_ACCESS_KEY', None)
+    os.environ.pop('AWS_DEFAULT_REGION', None)
